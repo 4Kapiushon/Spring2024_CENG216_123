a2df50916e80de2e5de8f4105500a8f7b03e21fa
4Kapiushon <163037822+4Kapiushon@users.noreply.github.com>
2024-05-24 18:17:44 +0300

    Add files via upload

---
+++ src/com/longluo/webchat/Constants.java
@@ -0,0 +1,59 @@
+package com.longluo.webchat;
+
+public class Constants {
+    // The Strings
+    public static final String APP_NAME = "Chat Room";
+    public static final String APP_NAME_STR = "Instant Messaging Chat Room";
+    public static final String APP_SERVER_NAME = "Chat Room Chat Server";
+
+    public static final int SERVER_PORT = 8888;
+
+    public static final String LOGIN = "Login";
+    public static final String REGISTER = "register";
+    public static final String EXIT = "quit";
+
+    public static final String SEND = "send";
+    public static final String SPEAK = "speak:";
+    public static final String SAVE = "保存";
+
+    public static final String SERVER_IP = "Server IP:";
+    public static final String USER_NAME = "    username:";
+    public static final String PASSWORD = "     password:  ";
+
+    public static final String USER = "user";
+
+    public static final String SERVER = "server";
+    public static final String LOG = "log";
+
+    public static final String SERVER_NAME = "name of server:";
+    public static final String SERVER_PORT_DESC = "Server port:";
+
+    public static final String CLOSE_SERVER = "Shut down the server(C)";
+
+    public static final String SERVER_LOG = "[Server log]";
+    public static final String SAVE_LOG = "save log(S)]";
+    public static final String FONT_SONG = "Song style";
+
+    public static final String SERVER_ADMIN = SERVER + " Manage";
+    public static final String ONLINE_USERS = "Online Users";
+    public static final String ONLINE_USERS_NUM = "No. of People:";
+    public static final String ONLINE_USERS_LIST = "[Online user list]";
+    public static final String ABOUT_APP = "About";
+    public static final String ZERO_NUM = "0 people";
+
+    public static final String HAD_EXIT = "Already exited, ";
+    public static final String EXIT_TIME = "Exit time:";
+    public static final String LEAVE_ROOM = " Left the chat room";
+
+    public static final String VERSION_INFO = "Member ID    Student ID   First Name               Last Name\n" +
+            "\n" +
+            "1            220218366    Arsalan Akbar            Tunio\n" +
+            "2            220212308    Faseeh Ul Din            Bilal\n" +
+            "3            220212324    Alexander                Kobzev\n" +
+            "4            210218322    Abdalla Hamdi Fathi      Mohamed\n" +
+            "5            220218351    Noureddine               Honeini\n" +
+            "6            220218449    Abdullah Naji Hasan      Al-Salami\n";
+    public static final String APP_ABOUT_INFO = "Instant Messaging Chat Room";
+
+    public static final String SAVE_LOG_FILE = "Records are saved in log.txt";
+}
